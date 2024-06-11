c22a090f59387dfbf2317292c330c7de9734efc5
Patrice-E <eydaleine.patrice@orange.fr>
2024-06-11 07:51:12 +0200

    Ajout persist zustand

--- src/store.ts
+++ src/store.ts
@@ -1,28 +1,31 @@
 import { create } from 'zustand';
-import { combine } from 'zustand/middleware';
+import { combine, persist } from 'zustand/middleware';
 import { Schema } from './components/createEmployeeForm/schema';
 
 const useAppStore = create(
-  combine(
-    {
-      users: [] as Schema[],
-    },
-    (set) => ({
-      addUser: (user: Schema) =>
-        set((state) => {
-          state.users.push(user);
-          return state;
-        }),
-      removeUser: (user: Schema) => {
-        set((state) => {
-          const newArray = state.users.filter(
-            (u) => u.lastName !== user.lastName
-          );
-          state.users = newArray;
-          return state;
-        });
+  persist(
+    combine(
+      {
+        users: [] as Schema[],
       },
-    })
+      (set) => ({
+        addUser: (user: Schema) =>
+          set((state) => {
+            state.users.push(user);
+            return state;
+          }),
+        removeUser: (user: Schema) => {
+          set((state) => {
+            const newArray = state.users.filter(
+              (u) => u.lastName !== user.lastName
+            );
+            state.users = newArray;
+            return state;
+          });
+        },
+      })
+    ),
+    { name: 'employees' }
   )
 );
 
